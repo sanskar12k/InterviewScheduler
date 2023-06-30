9d2312f85cbe2c07d34767a08457f507a3023a2a
Abhas-15 <abhirup.abhas@gmail.com>
2023-06-30 17:43:46 +0530

    feat:excel to table

--- src/scenes/bar/index.jsx
+++ src/scenes/bar/index.jsx
@@ -1,16 +1,52 @@
-import { Box } from "@mui/material";
-import Header from "../../components/Header";
-import BarChart from "../../components/BarChart";
+import { useState } from "react";
+import * as XLSX from "xlsx";
 
-const Bar = () => {
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  const handleFileUpload = (e) => {
+    const reader = new FileReader();
+    reader.readAsBinaryString(e.target.files[0]);
+    reader.onload = (e) => {
+      const data = e.target.result;
+      const workbook = XLSX.read(data, { type: "binary" });
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+      const parsedData = XLSX.utils.sheet_to_json(sheet);
+      setData(parsedData);
+    };
+  };
+
   return (
-    <Box m="20px">
-      <Header title="Bar Chart" subtitle="Simple Bar Chart" />
-      <Box height="75vh">
-        <BarChart />
-      </Box>
-    </Box>
+    <div className="App">
+      <input type="file" accept=".xlsx, .xls" onChange={handleFileUpload} />
+      {data.length > 0 && (
+        <table className="table">
+          <thead>
+            <tr>
+              {Object.keys(data[0]).map((key) => (
+                <th key={key}>{key}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, index) => (
+              <tr key={index}>
+                {Object.values(row).map((value, index) => (
+                  <td key={index}>{value}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+      <br />
+      <br />
+      ... webstylepress ...
+    </div>
   );
-};
+}
 
-export default Bar;
+export default App;
